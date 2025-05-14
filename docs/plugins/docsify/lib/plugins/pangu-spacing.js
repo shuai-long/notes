95094915db9511cd6249736c2db3716b8ed05bc4
shuai-long <77817836+shuai-long@users.noreply.github.com>
2025-05-14 20:14:08 +0800

    css

---
+++ docs/plugins/docsify/lib/plugins/pangu-spacing.js
@@ -0,0 +1,20 @@
+(window.$docsify = window.$docsify || {}),
+  (window.$docsify.plugins = [].concat(function (hook, vm) {
+    hook.doneEach(function () {
+      // 中英文添加空格
+      // pangu.spacingElementById('main');
+      // pangu.spacingElementByClassName('comment');
+      // pangu.spacingElementByTagName('p');
+
+        pangu.spacingElementByTagName('aside');
+        pangu.spacingElementByTagName('h2');
+        pangu.spacingElementByTagName('h3');
+        pangu.spacingElementByTagName('h4');
+        pangu.spacingElementByTagName('h5');
+        pangu.spacingElementByTagName('h6');
+        pangu.spacingElementByTagName('p');
+        pangu.spacingElementByTagName('button');
+        pangu.autoSpacingPage();
+      
+    });
+  }, window.$docsify.plugins));
