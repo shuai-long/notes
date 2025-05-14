e7b55d6a40375f440d44a5a3656fcfa6ff4d8173
shuai-long <77817836+shuai-long@users.noreply.github.com>
2025-05-14 21:51:38 +0800

    Update pangu-spacing.js

--- docs/plugins/docsify/lib/plugins/pangu-spacing.js
+++ docs/plugins/docsify/lib/plugins/pangu-spacing.js
@@ -1,39 +1,3 @@
-// (window.$docsify = window.$docsify || {}),
-//   (window.$docsify.plugins = [].concat(function (hook, vm) {
-//     hook.ready(function () {
-//       // 中英文添加空格
-//       // pangu.spacingElementById('main');
-//       // pangu.spacingElementByClassName('comment');
-//       // pangu.spacingElementByTagName('p');
-
-//       // pangu.spacingElementByTagName('aside');
-//       // pangu.spacingElementByTagName('h2');
-//       // pangu.spacingElementByTagName('h3');
-//       // pangu.spacingElementByTagName('h4');
-//       // pangu.spacingElementByTagName('h5');
-//       // pangu.spacingElementByTagName('h6');
-//       // pangu.spacingElementByTagName('p');
-//       // pangu.spacingElementByTagName('button');
-//       // pangu.autoSpacingPage();
-
-//       let is_spacing = false;
-
-//       is_spacing = true;
-//       pangu.spacingPage();
-//       is_spacing = false;
-
-//       document.addEventListener('DOMContentLoaded', () => {
-//         if (!is_spacing) {
-//           is_spacing = true;
-//           pangu.spacingNode(e.target);
-//           is_spacing = false;
-//         }
-//       });
-
-//     });
-//   }, window.$docsify.plugins));
-
-
 (window.$docsify = window.$docsify || {}),
   (window.$docsify.plugins = [].concat(function (hook, vm) {
     let is_spacing = false;
@@ -42,7 +6,9 @@
     hook.doneEach(function () {
       if (!is_spacing) {
         is_spacing = true;
+        const start = Date.now();
         pangu.spacingPage();
+        console.log(`排版耗时：${Date.now() - start}ms`);
         is_spacing = false;
       }
     });
